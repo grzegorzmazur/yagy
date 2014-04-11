2c437512f059f45133b3a8589581708b36671d01
mnoga <martanoga@users.noreply.github.com>
2014-04-11 16:48:55 +0000

    Initial version

---
+++ mainwindow.cpp
@@ -0,0 +1,67 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QtWebKitWidgets/QWebPage>
+#include <QtWebKitWidgets/QWebFrame>
+#include <QFile>
+
+#ifdef __APPLE__
+#include <CoreFoundation/CoreFoundation.h>
+#endif
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+
+    ui->setupUi(this);
+    loadYacasPage();
+    setUnifiedTitleAndToolBarOnMac(true);
+}
+
+void MainWindow::loadYacasPage()
+{
+
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
+    CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+    
+    char path[PATH_MAX];
+    if (!CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX))
+    {
+        qDebug() << "Error finding Resources URL";
+    }
+    
+    QString resourcesPath( path );
+    
+    QFile mFile(":/resources/view.html");
+    
+    if(!mFile.open(QFile::ReadOnly | QFile::Text)){
+        qDebug() << "could not open file for read (view.html)";
+        return;
+    }
+    
+    QTextStream in(&mFile);
+    QString mText = in.readAll();
+    mFile.close();
+
+    ui->webView->setHtml( mText, QUrl("file://"+ resourcesPath + "/")) ;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void
+MainWindow::changeEvent(QEvent *e)
+{
+    QMainWindow::changeEvent(e);
+    switch (e->type()) {
+        case QEvent::LanguageChange:
+            ui->retranslateUi(this);
+            break;
+        default:
+            break;
+    }
+}
+
