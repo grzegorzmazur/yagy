ddc33b304ec24288235b15ee925b386a7c7e89b0
grzesiek <grzegorzmazur@users.noreply.github.com>
2014-04-11 18:44:37 +0000

    icon and splash screen

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
-#include <QApplication>
 #include "mainwindow.h"
 
-#include <iostream>
-
-using namespace std;
-
+#include <QApplication>
+#include <QBitmap>
+#include <QPixmap>
+#include <QSplashScreen>
+#include <QTimer>
 
 
 int main(int argc, char *argv[]){
 	QApplication app(argc, argv);
     
 	MainWindow  *widget = new MainWindow ();
+        
+        QPixmap si(":/img/splash.png");
+        QSplashScreen ss(widget, si, Qt::WindowStaysOnTopHint);
+        ss.setMask(si.mask());
+        ss.showFullScreen();
+        QTimer::singleShot(3000, &ss, SLOT(close()));
+
+        
 	widget->show();
     
 	return app.exec();
