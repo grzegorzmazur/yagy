1d59eecfa72e78d1c1ba2e181d903ffd46838ecb
Grzegorz Mazur <teoretyk@gmail.com>
2014-04-21 12:15:38 +0000

    calculations moved to a separate thread

---
+++ cellproxy.cpp
@@ -0,0 +1,123 @@
+#include "cellproxy.h"
+
+#include <QtCore/QVariant>
+#include <QtCore/QJsonDocument>
+
+CellProxy::CellProxy(QWebFrame* frame, int idx, QString expr, YacasServer& yacas_server, CYacas& yacas2tex, QObject* parent):
+    QObject(parent),
+    _frame(frame),
+    _idx(idx),
+    _expr(expr),
+    _yacas_server(yacas_server),
+    _yacas2tex(yacas2tex),
+    _request(new YacasRequest(expr + ";"))
+{
+    connect(_request, SIGNAL(state_changed(YacasRequest::State)), this, SLOT(on_request_state_changed(YacasRequest::State)));
+    _yacas_server.submit(_request);
+}
+
+CellProxy::~CellProxy()
+{
+    delete _request;
+}
+
+void CellProxy::on_request_state_changed(YacasRequest::State state)
+{
+    if (state == YacasRequest::READY) {
+        QVariantMap evaluation_result;
+        evaluation_result["idx"] = _idx;
+        evaluation_result["input"] = _expr;
+        
+        if (!_request->side_effects().isEmpty())
+            evaluation_result["side_effects"] = _request->side_effects();
+        
+        switch (_request->result_type()) {
+            case YacasRequest::EXPRESSION: {
+                const QString result = _request->result().trimmed();
+                const QString texform_expr = QString("TeXForm(Hold(") + result + "));";
+                _yacas2tex.Evaluate(texform_expr.toStdString().c_str());
+                const QString texform_result = _yacas2tex.Result();
+                const QString tex_code =
+                    texform_result.trimmed().mid(2, texform_result.length() - 5);
+                evaluation_result["type"] = "Expression";
+                evaluation_result["expression"] = result;
+                evaluation_result["tex_code"] = tex_code;
+
+                break;
+            }
+            case YacasRequest::PLOT2D: {
+                
+                QList<QVariant> data;
+
+                const QString result = _request->result().trimmed();
+                QStringList parts = result.split("}},{{");
+
+                QString options_string = parts.takeLast().trimmed();
+                options_string.truncate(options_string.length() - 2);
+
+                QRegExp dict_entry_rx("(\"[^\"]+\"),(.+)");
+                QRegExp number_list_rx("\\{([^,\\}]+)(?:,([^,\\}]+))*\\}");
+                QRegExp string_list_rx("\\{(?:\"([^\"]+)\")(?:,\"([^\"]+)\")*\\}");
+
+                QStringList labels;
+
+                foreach (QString os, options_string.split("},{")) {
+
+                    dict_entry_rx.exactMatch(os);
+
+                    if (dict_entry_rx.cap(1) == "\"xrange\"") {
+                        number_list_rx.exactMatch(dict_entry_rx.cap(2));
+                    }
+
+                    if (dict_entry_rx.cap(1) == "\"yname\"") {
+                        string_list_rx.exactMatch(dict_entry_rx.cap(2));
+                        for (int i = 1; i <= string_list_rx.captureCount(); ++i)
+                            labels.append(string_list_rx.cap(i));
+                    }
+                }
+
+                parts = parts.replaceInStrings("{{{", "");
+                parts = parts.replaceInStrings("}}}", "");
+
+                for (int i = 0; i < parts.size(); ++i) {
+
+                    QList<QVariant> partial_data;
+
+                    foreach (const QString& ss, parts[i].split("},{")) {
+                        QList<QVariant> p;
+                        foreach (QString s, ss.split(",")) {
+                            p.append(s.replace("{", "").replace("}","").toDouble());
+                        }
+                        partial_data.append(QVariant(p));
+                    }
+
+                    QVariantMap data_entry;
+                    data_entry["label"] = labels[i];
+                    data_entry["data"] = partial_data;
+                    data.append(data_entry);
+                }
+
+                evaluation_result["type"] = "Plot2D";
+                evaluation_result["plot2d_data"] = data;
+
+                
+                break;
+            }
+            case YacasRequest::PLOT3D: {
+                evaluation_result["type"] = "Error";
+                evaluation_result["error_message"] = "Not implemented";
+                break;
+            }
+            case YacasRequest::ERROR: {
+                evaluation_result["type"] = "Error";
+                evaluation_result["error_message"] = _request->result().trimmed();
+
+                break;
+            }
+        }
+        
+        _frame->evaluateJavaScript(QString("printResults(") + (QJsonDocument::fromVariant(evaluation_result)).toJson() + ");");
+        
+        deleteLater();
+    }
+}
--- yacasengine.cpp
+++ yacasengine.cpp
@@ -2,6 +2,11 @@
 
 #include <QMutexLocker>
 
+#ifdef __APPLE__
+#include <CoreFoundation/CoreFoundation.h>
+#endif
+
+#include "config.h"
 
 YacasEngine::YacasEngine(YacasRequestQueue& requests, QObject* parent):
     QObject(parent),
@@ -9,8 +14,29 @@ YacasEngine::YacasEngine(YacasRequestQueue& requests, QObject* parent):
     _yacas(new CYacas(new StringOutput(_side_effects))),
     _idx(1)
 {
-    _yacas->Evaluate("DefaultDirectory(\"/home/mazur/work/yacas/trunk-root/share/yacas/scripts/\");");
+#ifdef __APPLE__
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
+    CFURLRef frameworkURL = CFBundleCopySharedFrameworksURL (mainBundle);
+    char path[PATH_MAX];
+    if (!CFURLGetFileSystemRepresentation(frameworkURL, TRUE, (UInt8 *)path, PATH_MAX))
+    {
+        qDebug() << "Error finding Resources URL";
+    }
+
+    _yacas->Evaluate((std::string("DefaultDirectory(\"") + std::string(path) + std::string("/yacas.framework/Versions/Current/Resources/scripts/\");")).c_str());
+#else
+    _yacas->Evaluate((std::string("DefaultDirectory(\"") + std::string(YACAS_PREFIX) + std::string("/share/yacas/scripts/\");")).c_str());
+#endif
     _yacas->Evaluate("Load(\"yacasinit.ys\");");
+
+    _yacas->Evaluate("Plot2D'outputs();");
+    
+    _yacas->Evaluate("Plot2D'yagy(values_IsList, _options'hash) <-- Yagy'Plot2D'Data(values, options'hash);");
+    _yacas->Evaluate("Plot2D'outputs() := { {\"default\", \"yagy\"}, {\"data\", \"Plot2D'data\"}, {\"gnuplot\", \"Plot2D'gnuplot\"}, {\"java\", \"Plot2D'java\"}, {\"yagy\", \"Plot2D'yagy\"}, };");
+    
+    _yacas->Evaluate("Plot3DS'outputs();");
+    _yacas->Evaluate("Plot3DS'yagy(values_IsList, _options'hash) <-- Yagy'Plot3DS'Data(values, options'hash);");
+    _yacas->Evaluate("Plot3DS'outputs() := { {\"default\", \"yagy\"}, {\"data\", \"Plot3DS'data\"}, {\"gnuplot\", \"Plot3DS'gnuplot\"}, {\"yagy\", \"Plot3DS'yagy\"},};");
 }
 
 YacasEngine::~YacasEngine()
@@ -45,8 +71,17 @@ void YacasEngine::on_start_processing()
 
             if (!_yacas->IsError()) {
                 QString result = _yacas->Result();
-                result = result.left(result.length() - 1);
-                request->answer(_idx++, YacasRequest::EXPRESSION, result.trimmed(), QString(_side_effects.c_str()));
+                result = result.left(result.length() - 1).trimmed();
+                
+                YacasRequest::ResultType result_type = YacasRequest::EXPRESSION;
+                if (result.startsWith("Yagy'Plot2D'Data")) {
+                    result_type = YacasRequest::PLOT2D;
+                    result = result.remove("Yagy'Plot2D'Data(");
+                    result.truncate(result.length() - 1);
+                } else if (result.startsWith("Yagy'Plot3DS'Data"))
+                    result_type = YacasRequest::PLOT3D;
+
+                request->answer(_idx++, result_type, result, QString(_side_effects.c_str()));
             } else {
                 QString msg = _yacas->Error();
                 request->answer(_idx++, YacasRequest::ERROR, msg.trimmed(), QString(_side_effects.c_str()));
