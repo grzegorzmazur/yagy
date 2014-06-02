974fa4ed13eb98006722536cfe370d2c130a5361
grzesiek <grzegorzmazur@users.noreply.github.com>
2014-06-02 14:13:50 +0000

    3d plotting moved to a separate class

--- javascript/slot.js
+++ javascript/slot.js
@@ -1,3 +1,250 @@
+function Plot3D(series, w, h) {
+    var self = this;
+    
+    var data = series[0]["data"];
+
+    self.xmin = data[0][0];
+    self.xmax = data[0][0];
+
+    self.ymin = data[0][1];
+    self.ymax = data[0][1];
+
+    self.zmin = data[0][2];
+    self.zmax = data[0][2];
+
+    for (var i = 1; i < data.length; ++i) {
+        if (data[i][0] < self.xmin)
+            self.xmin = data[i][0];
+        if (data[i][0] > self.xmax)
+            self.xmax = data[i][0];
+        if (data[i][1] < self.ymin)
+            self.ymin = data[i][1];
+        if (data[i][1] > self.ymax)
+            self.ymax = data[i][1];
+        if (data[i][2] < self.zmin)
+            self.zmin = data[i][2];
+        if (data[i][2] > self.zmax)
+            self.zmax = data[i][2];
+    }
+
+    self.size = 500;
+
+    self.xscale = self.size / (self.xmax - self.xmin);
+    self.yscale = self.size / (self.ymax - self.ymin);
+    self.zscale = self.size / (self.zmax - self.zmin);
+
+    self.xoffset = -self.size / 2;
+    self.yoffset = -self.size / 2;
+    self.zoffset = -self.size / 2;
+
+    self.scene = new THREE.Scene();
+
+    self.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 2000);
+    self.camera.position.z = 1000;
+
+    var geometry = new THREE.Geometry();
+
+    for (var i = 0; i < data.length - 2; ++i) {
+        var p0 = data[i];
+        var p1 = data[i + 1];
+
+        if (p0[0] !== p1[0])
+            continue;
+
+        var p2 = null;
+        var p3 = null;
+
+        for (var j = i + 2; j < data.length; ++j) {
+            if (p2 === null && data[j][1] == p0[1])
+                p2 = data[j];
+
+            if (p3 === null && data[j][1] == p1[1])
+                p3 = data[j];
+
+            if (p2 !== null && p3 !== null)
+                break;
+        }
+
+        if (p2 === null)
+            continue;
+
+        geometry.vertices.push(self.g2w(p0[0], p0[1], p0[2]));
+        geometry.vertices.push(self.g2w(p1[0], p1[1], p1[2]));
+        geometry.vertices.push(self.g2w(p2[0], p2[1], p2[2]));
+
+        geometry.faces.push(new THREE.Face3(geometry.vertices.length - 3, geometry.vertices.length - 2, geometry.vertices.length - 1));
+
+        if (p3 === null)
+            continue;
+
+        geometry.vertices.push(self.g2w(p3[0], p3[1], p3[2]));
+
+        // note: the orientation of the face is opposite to the previous one
+        geometry.faces.push(new THREE.Face3(geometry.vertices.length - 3, geometry.vertices.length - 2, geometry.vertices.length - 1));
+    }
+
+    var material = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        wireframe: true,
+        side: THREE.DoubleSide
+    });
+    var mesh = new THREE.Mesh(geometry, material);
+
+    self.scene.add(mesh);
+
+    material = new THREE.LineBasicMaterial({color: 0x000000});
+    geometry = new THREE.Geometry();
+
+    geometry.vertices.push(new THREE.Vector3(-self.size / 2, -self.size / 2, -self.size / 2));
+    geometry.vertices.push(new THREE.Vector3(self.size / 2, -self.size / 2, -self.size / 2));
+
+    var line = new THREE.Line(geometry, material);
+
+    self.scene.add(line);
+
+    geometry = new THREE.Geometry();
+
+    geometry.vertices.push(new THREE.Vector3(-self.size / 2, -self.size / 2, -self.size / 2));
+    geometry.vertices.push(new THREE.Vector3(-self.size / 2, self.size / 2, -self.size / 2));
+
+    line = new THREE.Line(geometry, material);
+
+    self.scene.add(line);
+
+    geometry = new THREE.Geometry();
+
+    geometry.vertices.push(new THREE.Vector3(-self.size / 2, -self.size / 2, -self.size / 2));
+    geometry.vertices.push(new THREE.Vector3(-self.size / 2, -self.size / 2, self.size / 2));
+
+    var line = new THREE.Line(geometry, material);
+
+    self.scene.add(line);
+
+    var x_params = self.axis_params(self.xmin, self.xmax);
+
+    for (var t = x_params.b; t <= x_params.e; t += x_params.d) {
+        var tb = self.g2w(t, self.ymin, self.zmin);
+        tb.y = -self.size / 2;
+        tb.z = -self.size / 2;
+        var te = tb.clone();
+        te.y += self.size / 50;
+        geometry = new THREE.Geometry();
+        geometry.vertices.push(tb);
+        geometry.vertices.push(te);
+        te = tb.clone();
+        te.z += self.size / 50;
+        geometry.vertices.push(tb);
+        geometry.vertices.push(te);
+        var line = new THREE.Line(geometry, material);
+        self.scene.add(line);
+
+        var tn = new Number(t);
+        var l = self.label(tn.toFixed(2));
+        l.position.set(tb.x, -self.size / 2, -self.size / 2);
+        self.scene.add(l);
+    }
+
+    var y_params = self.axis_params(self.ymin, self.ymax);
+
+    for (var t = y_params.b; t <= y_params.e; t += y_params.d) {
+        var tb = self.g2w(self.xmin, t, self.zmin);
+        tb.x = -self.size / 2;
+        tb.z = -self.size / 2;
+        var te = tb.clone();
+        te.x += self.size / 50;
+        geometry = new THREE.Geometry();
+        geometry.vertices.push(tb);
+        geometry.vertices.push(te);
+        te = tb.clone();
+        te.z += self.size / 50;
+        geometry.vertices.push(tb);
+        geometry.vertices.push(te);
+        var line = new THREE.Line(geometry, material);
+        self.scene.add(line);
+
+        if (t != y_params.b) {
+            var tn = new Number(t);
+            var l = self.label(tn.toFixed(2));
+            l.position.set(-self.size / 2, tb.y, -self.size / 2);
+            self.scene.add(l);
+        }
+    }
+
+    var z_params = self.axis_params(self.zmin, self.zmax);
+
+    for (var t = z_params.b; t <= z_params.e; t += z_params.d) {
+        var tb = self.g2w(self.xmin, self.ymin, t);
+        tb.x = -self.size / 2;
+        tb.y = -self.size / 2;
+        var te = tb.clone();
+        te.x += self.size / 50;
+        geometry = new THREE.Geometry();
+        geometry.vertices.push(tb);
+        geometry.vertices.push(te);
+        te = tb.clone();
+        te.y += self.size / 50;
+        geometry.vertices.push(tb);
+        geometry.vertices.push(te);
+        var line = new THREE.Line(geometry, material);
+        self.scene.add(line);
+
+        var tn = new Number(t);
+        var l = self.label(tn.toFixed(2));
+        l.position.set(-self.size / 2, -self.size / 2, tb.z + 40);
+        self.scene.add(l);
+    }
+
+    self.renderer = new THREE.WebGLRenderer();
+    self.renderer.setClearColor(0xffffff, 1);
+    self.renderer.setSize(w, h);
+}
+
+Plot3D.prototype.g2w = function (x, y, z) {
+    return new THREE.Vector3((x - this.xmin) * this.xscale + this.xoffset,  (y - this.ymin) * this.yscale + this.yoffset, (z - this.zmin) * this.zscale + this.zoffset);
+}
+
+
+Plot3D.prototype.axis_params = function (min, max) {
+    var delta = max - min;
+
+    var scale = Math.pow(10, Math.floor((Math.round(Math.log(delta) / Math.LN10 * 1e6) / 1e6) - 1));
+    var b = Math.ceil(min / scale) * scale;
+    var e = Math.floor(max / scale) * scale;
+    var d = Math.floor((e - b) / (10 * scale)) * scale;
+
+    return { b: b, e: e, d: d };
+}
+
+Plot3D.prototype.label = function (text) {
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    var font_size = 256;
+    context.font = "normal " + font_size + "px Arial";
+
+    var text_width = context.measureText(text).width;
+    var text_height = 200;
+
+    canvas.width = text_width;
+
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    context.fillStyle = "#000000";
+    context.fillText(text, text_width / 2, text_height / 2);
+
+    var texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+
+    var material = new THREE.SpriteMaterial({
+        map: texture,
+        useScreenCoordinates: false,
+        transparent: true
+    });
+
+    var sprite = new THREE.Sprite(material);
+    sprite.scale.set(text_width / text_height * font_size, font_size, 1);
+
+    return sprite;
+}
 
 function load(){
     //$("#inputExpression").focus();
@@ -107,286 +354,27 @@ function printResults( result ){
         $("#" + outputID).resizable({ minWidth: 200 });
         $("#" + outputID).resizable({ minHeight: 200 });
     }else if( result["type"] == "Plot3D" ){
+
+        var width = $("#" + outputID).parent().width();
+        var height = 200;
+
+        $("#" + outputID).resizable({ maxWidth: width });
+        $("#" + outputID).resizable({ minWidth: 200 });
+        $("#" + outputID).resizable({ minHeight: height });
         
-        var data = result["plot3d_data"][0]["data"]; 
+        var plot3d = new Plot3D(result["plot3d_data"], width, height);
         
-        var xmin = data[0][0];
-        var xmax = data[0][0];
-        
-        var ymin = data[0][1];
-        var ymax = data[0][1];
+        $("#" + outputID).append(plot3d.renderer.domElement);
 
-        var zmin = data[0][2];
-        var zmax = data[0][2];
-        
-        for (var i = 1; i < data.length; ++i) {
-            if (data[i][0] < xmin)
-                xmin = data[i][0];
-            if (data[i][0] > xmax)
-                xmax = data[i][0];
-            if (data[i][1] < ymin)
-                ymin = data[i][1];
-            if (data[i][1] > ymax)
-                ymax = data[i][1];
-            if (data[i][2] < zmin)
-                zmin = data[i][2];
-            if (data[i][2] > zmax)
-                zmax = data[i][2];
-        }
-
-        var size = 500;
-
-        var xscale = size / (xmax - xmin);
-        var yscale = size / (ymax - ymin);
-        var zscale = size / (zmax - zmin);
-
-        var xoffset = -size / 2;
-        var yoffset = -size / 2;
-        var zoffset = -size / 2;
-
-        var scene, camera, renderer;
-        var controls;
-
-        init();
-        render();
-
-        function g2w(x, y, z) {
-            
-            var size = 500;
-
-            var xscale = size / (xmax - xmin);
-            var yscale = size / (ymax - ymin);
-            var zscale = size / (zmax - zmin);
-
-            var xoffset = -size / 2;
-            var yoffset = -size / 2;
-            var zoffset = -size / 2;
-            
-            return new THREE.Vector3((x - xmin) * xscale + xoffset,  (y - ymin) * yscale + yoffset, (z - zmin) * zscale + zoffset);
-        }
-
-        function axis_params(min, max) {
-            var delta = max - min;
-
-            var scale = Math.pow(10, Math.floor((Math.round(Math.log(delta) / Math.LN10 * 1e6) / 1e6) - 1));
-            var b = Math.ceil(min / scale) * scale;
-            var e = Math.floor(max / scale) * scale;
-            var d = Math.floor((e - b) / (10 * scale)) * scale;
-            
-            return { b: b, e: e, d: d };
-        }
-
-        function label(text) {
-            var canvas = document.createElement('canvas');
-            var context = canvas.getContext('2d');
-            var font_size = 256;
-            context.font = "normal " + font_size + "px Arial";
-            
-            var text_width = context.measureText(text).width;
-            var text_height = 200;
-            
-            canvas.width = text_width;
-            
-            context.textAlign = "center";
-            context.textBaseline = "middle";
-            context.fillStyle = "#000000";
-            context.fillText(text, text_width / 2, text_height / 2);
-            
-            var texture = new THREE.Texture(canvas);
-            texture.needsUpdate = true;
-            
-            var material = new THREE.SpriteMaterial({
-                map: texture,
-                useScreenCoordinates: false,
-                transparent: true
-            });
-
-            var sprite = new THREE.Sprite(material);
-            sprite.scale.set(text_width / text_height * font_size, font_size, 1);
-
-            return sprite;
-        }
-
-        function init() {
-            
-            scene = new THREE.Scene();
-
-            camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 2000 );
-            camera.position.z = 1000;
-
-            var geometry = new THREE.Geometry();
-
-            for (var i = 0; i < data.length - 2; ++i) {
-                var p0 = data[i];
-                var p1 = data[i + 1];
-                
-                if (p0[0] !== p1[0])
-                    continue;
-                
-                var p2 = null;
-                var p3 = null;
-                
-                for (var j = i + 2; j < data.length; ++j) {
-                    if (p2 === null && data[j][1] == p0[1])
-                       p2 = data[j];
-                    
-                    if (p3 === null && data[j][1] == p1[1])
-                       p3 = data[j];
-                    
-                    if (p2 !== null && p3 !== null)
-                        break;
-                }
-                
-                if (p2 === null)
-                    continue;
-                
-                geometry.vertices.push(g2w(p0[0], p0[1], p0[2]));
-                geometry.vertices.push(g2w(p1[0], p1[1], p1[2]));
-                geometry.vertices.push(g2w(p2[0], p2[1], p2[2]));
-
-                geometry.faces.push( new THREE.Face3( geometry.vertices.length - 3, geometry.vertices.length - 2, geometry.vertices.length - 1 ) );
-                
-                if (p3 === null)
-                    continue;
-
-                geometry.vertices.push(g2w(p3[0], p3[1], p3[2]));
-
-                // note: the orientation of the face is opposite to the previous one
-                geometry.faces.push( new THREE.Face3( geometry.vertices.length - 3, geometry.vertices.length - 2, geometry.vertices.length - 1 ) );
-            }
-
-            geometry.computeBoundingSphere();
-
-            var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true, side: THREE.DoubleSide } );
-            var mesh = new THREE.Mesh( geometry, material );
-
-            scene.add( mesh );
-
-            material = new THREE.LineBasicMaterial( { color: 0x000000 } );            
-
-            geometry = new THREE.Geometry();
-            
-            geometry.vertices.push(new THREE.Vector3(-size / 2, -size / 2, -size / 2));
-            geometry.vertices.push(new THREE.Vector3( size / 2, -size / 2, -size / 2));
-
-            var line = new THREE.Line(geometry, material);
-            
-            scene.add(line);
-
-            geometry = new THREE.Geometry();
-            
-            geometry.vertices.push(new THREE.Vector3(-size / 2, -size / 2, -size / 2));
-            geometry.vertices.push(new THREE.Vector3(-size / 2,  size / 2, -size / 2));
-            
-            line = new THREE.Line(geometry, material);
-            
-            scene.add(line);
-
-            geometry = new THREE.Geometry();
-            
-            geometry.vertices.push(new THREE.Vector3(-size / 2, -size / 2, -size / 2));
-            geometry.vertices.push(new THREE.Vector3(-size / 2, -size / 2,  size / 2));
-            
-            var line = new THREE.Line(geometry, material);
-            
-            scene.add(line);
-
-            var x_params = axis_params(xmin, xmax);
-
-            for (var t = x_params.b; t <= x_params.e; t += x_params.d) {
-                var tb = g2w(t, ymin, zmin);
-                tb.y = -size / 2;
-                tb.z = -size / 2;
-                var te = tb.clone();
-                te.y += size / 50;
-                geometry = new THREE.Geometry();
-                geometry.vertices.push(tb);
-                geometry.vertices.push(te);
-                te = tb.clone();
-                te.z += size / 50;
-                geometry.vertices.push(tb);
-                geometry.vertices.push(te);
-                var line = new THREE.Line(geometry, material);
-                scene.add(line);
-                
-                var tn = new Number(t);
-                var l = label(tn.toFixed(2));
-                l.position.set(tb.x, -size / 2, -size / 2);
-                scene.add(l);
-            }
-
-            var y_params = axis_params(ymin, ymax);
-
-            for (var t = y_params.b; t <= y_params.e; t += y_params.d) {
-                var tb = g2w(xmin, t, zmin);
-                tb.x = -size / 2;
-                tb.z = -size / 2;
-                var te = tb.clone();
-                te.x += size / 50;
-                geometry = new THREE.Geometry();
-                geometry.vertices.push(tb);
-                geometry.vertices.push(te);
-                te = tb.clone();
-                te.z += size / 50;
-                geometry.vertices.push(tb);
-                geometry.vertices.push(te);
-                var line = new THREE.Line(geometry, material);
-                scene.add(line);
-
-                if (t != y_params.b) {
-                    var tn = new Number(t);
-                    var l = label(tn.toFixed(2));
-                    l.position.set(-size / 2, tb.y, -size / 2);
-                    scene.add(l);
-                }
-            }
-
-            var z_params = axis_params(zmin, zmax);
-
-            for (var t = z_params.b; t <= z_params.e; t += z_params.d) {
-                var tb = g2w(xmin, ymin, t);
-                tb.x = -size / 2;
-                tb.y = -size / 2;
-                var te = tb.clone();
-                te.x += size / 50;
-                geometry = new THREE.Geometry();
-                geometry.vertices.push(tb);
-                geometry.vertices.push(te);
-                te = tb.clone();
-                te.y += size / 50;
-                geometry.vertices.push(tb);
-                geometry.vertices.push(te);
-                var line = new THREE.Line(geometry, material);
-                scene.add(line);
-                
-                var tn = new Number(t);
-                var l = label(tn.toFixed(2));
-                l.position.set(-size / 2, -size / 2, tb.z + 40);
-                scene.add(l);
-            }
-
-            // blacklist MacOS until we find out the way to make WebGL work
-            // reliably, or, in the worst case, make this a configuration option
-            if (window.navigator.platform === 'MacIntel')
-                renderer = new THREE.CanvasRenderer();
-            else
-                renderer = new THREE.WebGLRenderer();
-            
-            renderer.setClearColor(0xffffff, 1);
-            
-            renderer.setSize( $("#" + outputID).parent().width(), 200 );
-            $("#" + outputID).resizable({ maxWidth: $("#" + outputID).parent().width() });
-            $("#" + outputID).resizable({ minWidth: 200 });
-            $("#" + outputID).resizable({ minHeight: 200 });
-            $("#" + outputID).append(renderer.domElement);
-            controls = new THREE.TrackballControls( camera, renderer.domElement );
-        }
+        var controls = new THREE.TrackballControls(plot3d.camera, plot3d.renderer.domElement);
 
         function render() {
-            requestAnimationFrame( render );
-            renderer.render( scene, camera );
+            requestAnimationFrame(render);
+            plot3d.renderer.render(plot3d.scene, plot3d.camera);
             controls.update();
         }
+
+        render();
     }
 }
 
