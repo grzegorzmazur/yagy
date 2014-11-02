25e0d0af47818d5159c8186d9518b176bd1194e9
grzesiek <grzesiek@a827e005-4c7c-4a60-92a7-c8f0aff715b2>
2014-11-02 15:18:42 +0000

    skeleton of the preferences dialog

---
+++ trunk/preferences_dialog.h
@@ -0,0 +1,12 @@
+#ifndef PREFERENCES_DIALOG_H
+#define	PREFERENCES_DIALOG_H
+
+#include <QtWidgets/QDialog>
+
+class PreferencesDialog: public QDialog {
+public:
+    explicit PreferencesDialog(QWidget* parent = nullptr);
+};
+
+#endif
+
